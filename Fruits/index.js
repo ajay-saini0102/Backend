e26a9b4ad746b23704b0c7317166034c34ec31e2
Ajay Saini <153486378+ajaysaini3078@users.noreply.github.com>
2024-04-10 23:21:40 +0530

    add figlet packet using NPM

--- Fruits/index.js
+++ Fruits/index.js
@@ -1,6 +1,6 @@
 const apple = require("./apple");
 const banana = require("./banana");
-const Orange = require("./Orange");
+const Orange = require("./orange");
 
 let fruits = [apple, banana, Orange];
 module.exports = fruits;
